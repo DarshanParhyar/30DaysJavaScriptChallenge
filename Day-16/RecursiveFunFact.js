8e6348df27d186e1fb5ea20150354fecc2ec03ef
Darshan <darshankumar.parhyar@gmail.com>
2024-07-28 20:05:40 +0500

    Day 16 JS challenge completed

---
+++ Day-16/RecursiveFunFact.js
@@ -0,0 +1,12 @@
+//Task 1: Recursive function to calculate the factorial of a number
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+console.log(factorial(5));  
+
